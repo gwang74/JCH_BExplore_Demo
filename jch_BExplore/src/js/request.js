2bdf726cc563e2c6b78c3923997049d1f5994902
Astomsh <383063294@qq.com>
2019-06-20 14:27:02 +0800

    fix

--- jch_BExplore/src/js/request.js
+++ jch_BExplore/src/js/request.js
@@ -192,114 +192,109 @@ export const getLedgerInformationByIndex = async(index) => {
 // total_coins	swt总量
 // transaction_hash	交易hash树根
 // transactions	该账本里的交易列表
-<<
-<< << < HEAD
-export const getLedgerInformationByHash = async(hash) => { ===
-        === =
-        // transactionLength 交易数
-        export const getLedgerInformationByHash = async(hash) => { >>>
-            >>> > zcZhang
-            try {
-                let res = await api.get_ledger_information_by_hash(hash);
-                if (res.transactions) {
-                    res.transactionLength = res.transactions.length;
-                }
-                if (res.close_time_human) {
-                    res.close_time_human = formatUSATime(res.close_time_human);
-                }
-                return res;
-            } catch (error) {
-                return error;
-            }
+// transactionLength 交易数
+export const getLedgerInformationByHash = async(hash) => {
+    try {
+        let res = await api.get_ledger_information_by_hash(hash);
+        if (res.transactions) {
+            res.transactionLength = res.transactions.length;
         }
-
-        //获取最新区块高度
-        // hash 哈希
-        // ledger_index 高度
-        // transactionLength 交易数
-        // close_time_human 时间
-        export const getLedgerNew = async() => {
-            let ledger = await getLedgerIndex();
-            let ledgerNewData = [];
-            for (let i = 0; i < 6; i++) {
-                let data = await getLedgerInformationByIndex(ledger.ledger_index * 1 - i)
-                ledgerNewData.push(data)
-            }
-            return ledgerNewData;
+        if (res.close_time_human) {
+            res.close_time_human = formatUSATime(res.close_time_human);
         }
+        return res;
+    } catch (error) {
+        return error;
+    }
+}
 
-        //获取最新数据
-        // account 交易发起方
-        // amount 交易内容 {currency 币种名称， value 金额}
-        // counterparty 交易对家
-        // date 时间
-        // effects: []
-        // fee 燃料费用
-        // hash 哈希
-        // ledger 账本号
-        // memos 备注
-        // result 交易结果
-        // type 交易类型（见下备注）
-        export const getHomeData = async() => {
-            let ledger = await getLedgerIndex();
-            let ledgerData = await getLedgerInformationByIndex(ledger.ledger_index)
-            let hashDatas = ledgerData.transactions
-            let homeDatas = [];
-            if (hashDatas && hashDatas.length > 0) {
-                for (let hash of hashDatas) {
-                    homeDatas.push(await getTransactionsByHash(hash))
-                }
-            }
-            return homeDatas
+//获取最新区块高度
+// hash 哈希
+// ledger_index 高度
+// transactionLength 交易数
+// close_time_human 时间
+export const getLedgerNew = async() => {
+    let ledger = await getLedgerIndex();
+    let ledgerNewData = [];
+    for (let i = 0; i < 6; i++) {
+        let data = await getLedgerInformationByIndex(ledger.ledger_index * 1 - i)
+        ledgerNewData.push(data)
+    }
+    return ledgerNewData;
+}
+
+//获取最新数据
+// account 交易发起方
+// amount 交易内容 {currency 币种名称， value 金额}
+// counterparty 交易对家
+// date 时间
+// effects: []
+// fee 燃料费用
+// hash 哈希
+// ledger 账本号
+// memos 备注
+// result 交易结果
+// type 交易类型（见下备注）
+export const getHomeData = async() => {
+    let ledger = await getLedgerIndex();
+    let ledgerData = await getLedgerInformationByIndex(ledger.ledger_index)
+    let hashDatas = ledgerData.transactions
+    let homeDatas = [];
+    if (hashDatas && hashDatas.length > 0) {
+        for (let hash of hashDatas) {
+            homeDatas.push(await getTransactionsByHash(hash))
         }
+    }
+    return homeDatas
+}
 
 
-        // 关于交易类型
-        // type有如下几种：
+// 关于交易类型
+// type有如下几种：
 
-        // 1.sent，用户进行支付（转账）操作，交易信息中包含的信息有：
-        // counterparty	支付对家，即接收方
-        // amount	交易记录标记
-        // value	金额
-        // currency	货币
-        // issuer	货币发行方，SWT为空
-        // effects	详见effects解释
-
-
-        // 2.received，用户接受支付，在交易信息中包含的信息有：
-        // counterparty	支付对家，即接收方
-        // amount	交易记录标记
-        // value	金额
-        // currency	货币
-        // issuer	货币发行方，SWT为空
-        // effects	详见effects解释
+// 1.sent，用户进行支付（转账）操作，交易信息中包含的信息有：
+// counterparty	支付对家，即接收方
+// amount	交易记录标记
+// value	金额
+// currency	货币
+// issuer	货币发行方，SWT为空
+// effects	详见effects解释
 
 
-        // 3.convert，用户进行兑换操作，在交易信息中包含的信息有：
-        // spent	兑换支付的金额
-        // value	金额
-        // currency	货币
-        // issuer	货币发行方，SWT为空
-        // amount	交易记录标记
-        // value	金额
-        // currency	货币
-        // issuer	货币发行方，SWT为空
-        // effects	详见effects解释
+// 2.received，用户接受支付，在交易信息中包含的信息有：
+// counterparty	支付对家，即接收方
+// amount	交易记录标记
+// value	金额
+// currency	货币
+// issuer	货币发行方，SWT为空
+// effects	详见effects解释
 
 
-        // 4.offernew，用户进行挂单操作，在交易信息中包含的信息有：
-        // offertype	挂单类型，sell或buy
-        // pair	交易的货币对
-        // amount	挂单的数量
-        // price	挂单的价格
-        // effects	详见effects解释
+// 3.convert，用户进行兑换操作，在交易信息中包含的信息有：
+// spent	兑换支付的金额
+// value	金额
+// currency	货币
+// issuer	货币发行方，SWT为空
+// amount	交易记录标记
+// value	金额
+// currency	货币
+// issuer	货币发行方，SWT为空
+// effects	详见effects解释
 
-        // 5.offercancel，用户进行取消挂单操作，在交易信息中包含的信息有：
-        // type	挂单的类型，sell或buy
-        // pair	交易的货币对
-        // amount	挂单的数量
-        // price	挂单的价格
-        // effects	详见effects解释
 
-        // 6.offereffect，挂单成交情况，即被动成交的情况，在交易信息中包含的信息有：
-        // effects	详见effects解释
+// 4.offernew，用户进行挂单操作，在交易信息中包含的信息有：
+// offertype	挂单类型，sell或buy
+// pair	交易的货币对
+// amount	挂单的数量
+// price	挂单的价格
+// effects	详见effects解释
+
+// 5.offercancel，用户进行取消挂单操作，在交易信息中包含的信息有：
+// type	挂单的类型，sell或buy
+// pair	交易的货币对
+// amount	挂单的数量
+// price	挂单的价格
+// effects	详见effects解释
+
+// 6.offereffect，挂单成交情况，即被动成交的情况，在交易信息中包含的信息有：
+// effects	详见effects解释
